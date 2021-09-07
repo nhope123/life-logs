c499122f72f9deef1de442cccf882590c04fa25b
nhope <nhope@ryerson.ca>
2021-09-07 19:56:13 -0400

    Alternate Thought component

--- src/components/cbt-recorder/recordCreation/Evidence.js
+++ src/components/cbt-recorder/recordCreation/Evidence.js
@@ -3,62 +3,80 @@ import { ThoughtRecordContext } from '../../../contexts/ThoughtRecordContext'
 import Table from '../../Table'
 import SingleInputForm from '../SingleInputForm'
 import {v4 as uuidv4} from 'uuid'
+import NavigationButton from './NavigationButton'
 
 const EVIDENCE_FOR = 'This is true because:'
 const EVIDENCE_AGAINST = 'This is untrue because:'
 
 const EvidenceCell = (props) =>{
-  return (<span tabIndex={'0'} onClick={()=>props.callback}   >{props.thought}</span>)
+  return (<span tabIndex={'0'} onClick={props.callback}   >{props.thought}</span>)
 }
 
-//removeEvidence('for',forList[i].id)
-//{(forList[i])? forList[i].thought: ' '}
-
 const Evidence = () => {
-  const {automaticThought} = useContext(ThoughtRecordContext)
+  const {automaticThought, setEvidence} = useContext(ThoughtRecordContext)
   const [evidenceFor, setEvidenceFor] = useState([])
-  const [evidenceAgainst, setEvidenceAgainst] = useState([])
-
-  
+  const [evidenceAgainst, setEvidenceAgainst] = useState([])  
 
   const addEvidenceFor = value =>{
-    console.log(`for:${value}`);
     setEvidenceFor(state => [...state, {id: uuidv4(), thought: value}])
   }
+
   const addEvidenceAgainst = value =>{
-    console.log(`against:${value}`);
     setEvidenceAgainst(state => [...state, {id: uuidv4(), thought: value}])
   }
 
   const removeEvidence = (option, id) =>{
+    console.log('remove me');
     (option === 'for')? setEvidenceFor((state=> state.filter(item=> item.id !== id))):
                         setEvidenceAgainst((state=> state.filter(item=> item.id !== id)))
   }
 
-  const processEvidenceTableRows = (forList, againstList) =>{
-    const forLength = forList.length
-    const againstLength = againstList.length
+  const processEvidence = (option) =>{
+    const forLength = evidenceFor.length
+    const againstLength = evidenceAgainst.length
     let result = []
-    if(forLength === 0 && againstLength === 0) return result
-    for(let i = 0; i < Math.max(forLength,againstLength); i++){
 
-      /*{id , Evidence For, }  */
-      result.push({
-                    id: uuidv4(), 
-                    [EVIDENCE_FOR]: (forList[i])? <EvidenceCell {...{
-                                                                      thought: forList[i].thought, 
-                                                                      callback: removeEvidence('for',forList[i].id)
-                                                                    }} /> : ' ',
-                    [EVIDENCE_AGAINST]: (againstList[i])? <EvidenceCell {...{
-                                                                              thought: againstList[i].thought, 
-                                                                              callback: removeEvidence('against',againstList[i].id)
-                                                                            }} /> : ' '                    
-                  })
+    if(forLength === 0 && againstLength === 0 && option === 'table'){ return [{id: 1,[EVIDENCE_FOR]:' ',[EVIDENCE_AGAINST]:' ' }]}
+    else if(forLength === 0 && againstLength === 0 && option === 'context'){ return result }
+    else {
+      for(let i = 0; i < Math.max(forLength,againstLength); i++){
+        result.push((option === 'table')? {
+                      id: uuidv4(),
+                      [EVIDENCE_FOR]: (evidenceFor[i])? (<EvidenceCell {...{
+                                                                            thought: evidenceFor[i].thought, 
+                                                                            callback: ()=>removeEvidence('for',evidenceFor[i].id)
+                                                                          }}  /> ) : ' ',
+                      [EVIDENCE_AGAINST]: (evidenceAgainst[i])? (<EvidenceCell {...{
+                                                                                thought: evidenceAgainst[i].thought, 
+                                                                                callback: ()=>removeEvidence('against',evidenceAgainst[i].id)
+                                                                              }}  /> ) : ' '
+        } : [
+              (evidenceFor[i])? evidenceFor[i].thought : ' ',
+              (evidenceAgainst[i])? evidenceAgainst[i].thought: ' '
+        ])
+      }
     }
     return result
   }
 
-  let tableProps = {columns: [EVIDENCE_FOR, EVIDENCE_AGAINST], rows: processEvidenceTableRows(evidenceFor,evidenceAgainst)}
+  const updateEvidence = () =>{
+    const result = processEvidence('context')
+    result && setEvidence(result)
+  }
+  
+
+  let tableProps = {columns: [EVIDENCE_FOR, EVIDENCE_AGAINST], rows: processEvidence('table')}
+  
+  const directionProps ={
+    leftButton: {
+      callback: null,
+      title: 'Go Back to Automatic Thought'
+    },
+    rightButton: {
+      callback: ()=>updateEvidence(),
+      title: 'Go To Alternate Thought',
+    }
+  }
 
   return (
     <section >
@@ -68,6 +86,7 @@ const Evidence = () => {
       <Table {...tableProps} /> 
       <SingleInputForm {...{label: 'Statement that proves the title',callback: addEvidenceFor }}/>
       <SingleInputForm {...{label: 'Statement that disproves the title',callback: addEvidenceAgainst }} />
+      <NavigationButton {...directionProps} />
     </section >
   )
 }
--- src/contexts/ThoughtRecordContext.js
+++ src/contexts/ThoughtRecordContext.js
@@ -6,12 +6,14 @@ const ThoughtRecordContextProvider = (props) => {
   const [situation, setSituation] = useState('')
   const [emotion, setEmotion] = useState('Feeling Sad')
   const [automaticThoughts, setAutomaticThoughts] = useState('')
+  const [evidence, setEvidence] = useState([])
 
   return (
     <ThoughtRecordContext.Provider value={{
                                           situation, setSituation,
                                           emotion,setEmotion,
-                                          automaticThoughts,setAutomaticThoughts
+                                          automaticThoughts,setAutomaticThoughts,
+                                          evidence,setEvidence
     }} >
       {props.children}
     </ThoughtRecordContext.Provider >
